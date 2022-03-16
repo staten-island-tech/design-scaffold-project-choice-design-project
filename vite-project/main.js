4a044a0b1005f256c7faf7236950cf5899fb0048
raymondsun1129 <90789774+raymondsun1129@users.noreply.github.com>
2022-03-16 13:11:58 -0400

    c

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,3 +1,9 @@
 import './style.css'
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+const tl = gsap.timeline({ scrollTrigger: ".attractions", delay: 0.2});
 
+tl.from(".header-text", { opacity: 0, duration: 0.4 });
 
+tl.from(".attractions", { opacity: 0, duration: 0.2, ease:"ease-out" });
