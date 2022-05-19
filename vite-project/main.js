480e3effe47d2695b7e899a83be959839d07040d
raymondsun1129 <90789774+raymondsun1129@users.noreply.github.com>
2022-05-19 13:11:59 -0400

    C

--- vite-project/main.js
+++ vite-project/main.js
@@ -95,11 +95,10 @@ document.getElementById("change").addEventListener("click", function () {
 
 
 function myFunction() {
-  var x = document.getElementById("nav-id");
-  if (x.className === "nav") {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
     x.className += " responsive";
   } else {
-    x.className = "nav";
+    x.className = "topnav";
   }
 }
-
