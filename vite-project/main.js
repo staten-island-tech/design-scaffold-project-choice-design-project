6e5682017cd34594bab7a12fadb84c70de70cef8
raymondsun1129 <90789774+raymondsun1129@users.noreply.github.com>
2022-05-25 13:10:56 -0400

    c

--- vite-project/main.js
+++ vite-project/main.js
@@ -94,11 +94,11 @@ document.getElementById("change").addEventListener("click", function () {
  }
 });
 
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
+// function myFunction() {
+//   var x = document.getElementById("myTopnav");
+//   if (x.className === "topnav") {
+//     x.className += " responsive";
+//   } else {
+//     x.className = "topnav";
+//   }
+// }
